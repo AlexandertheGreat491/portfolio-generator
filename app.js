3841143e6d3a713c11f713eefd5c628490cc747b
agvan <agvandyke25@gmail.com>
2022-06-20 13:26:34 -0500

    modified the app.js & created the index.html files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
+/*const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
@@ -12,5 +14,34 @@ const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width-device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Portfolio Demo</title>
+  </head>
+
+  
+  <body>
+  <h1>${name}</h1>
+  <h2><a href="https://github.com/${github}">Github</a><h2>
+  </body>
+  </html>
+  `;
+  
+};
+
+//console.log(name, github);
+//console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+  console.log('Portfolio complete! Checkout index.html to see the output!');
+})
 
